8321b6959a231005ede1b66848ba51a968b0f4b4
Zachary Bush <zmbush@berkeley.edu>
2012-03-16 15:00:30 -0700

    hiddenbin change molly

--- hiddenbin.py
+++ hiddenbin.py
@@ -5,11 +5,4 @@ def _mary(ignore, env):
   return 'hello, mary'
 
 def _molly(ignore, env):
-  mail = env['mail']
-  ip = flask.request.remote_addr
-  msg = flaskext.mail.Message("Molly Command",
-                              sender="zach@zmbush.com",
-                              recipients=["zabu.other@gmail.com"])
-  msg.body="Someone executed the molly command from " + ip
-  mail.send(msg)
-  return "I love you, Molly"
+  return "Hello, Molly"
